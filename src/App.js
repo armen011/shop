928f40f9f5f498ba9a4e9e219e47dc53d14d72cc
Arturarakelyan1998 <Arturarakelyan089@gmail.com>
2022-04-04 22:35:05 +0400

    added qr reader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 function App() {
+  const [data, setData] = React.useState("Not Found");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{data}</p>
+    </>
   );
 }
 
